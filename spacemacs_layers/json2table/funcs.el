7cc69fc0d16c09cb2caa3e7e1315f17d92af780c
Jiang Qiqi <woshiqiqiye@gmail.com>
2016-05-30 08:46:03 +0800

    add functions

--- spacemacs_layers/json2table/funcs.el
+++ spacemacs_layers/json2table/funcs.el
@@ -1,3 +1,5 @@
+(require 'org)
+
 (defun funqiqi (args)
   "nothing"
   (interactive "P")
@@ -17,4 +19,29 @@
         (end (line-beginning-position 2)))
     beg)
   )
+
+(defun org-table-create-or-convert-from-region-json ()
+  "Convert region to table, or create an empty table.
+If there is an active region, convert it to a table, using the function
+`org-table-convert-region'.  See the documentation of that function
+to learn how the prefix argument is interpreted to determine the field
+separator.
+If there is no such region, create an empty table with `org-table-create'."
+  (interactive "P")
+  (if (org-region-active-p)
+      (org-table-convert-region-json (region-beginning) (region-end))
+    (org-table-create arg)))
+
+(defun org-table-convert-region-json (beg0 end0)
+  "Convert region to a table.
+   beginning-of-line is used to go to line header"
+  (interactive "r\nP")
+  (message "create table from json")
+  (let* ((beg (min beg0 end0))
+	 (end (max beg0 end0)) re)
+    (if
+(> (count-lines beg end) 99)
+(user-error "Region is longer than `org-table-convert-region-max-lines' (%s) lines; not converting"
+            99))
+))
 
